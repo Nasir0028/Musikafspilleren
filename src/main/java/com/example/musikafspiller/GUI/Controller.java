517122ce2116c2515f891f7290ee6513eed9b69d
Stinejenna <stinejenna99@gmail.com>
2024-11-28 12:31:53 +0100

    Opdateret Controller

--- src/main/java/com/example/musikafspiller/GUI/Controller.java
+++ src/main/java/com/example/musikafspiller/GUI/Controller.java
@@ -2,26 +2,40 @@ package com.example.musikafspiller.GUI;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 public class Controller {
 
     @FXML
     private Button addSong;
-Button addSongbutton = new Button("Add Song");
+    Button addSongbutton = new Button("Add Song");
+
+    @FXML
+    private ImageView albumCover;
 
     @FXML
     private Text artistPlay;
-Button artistPlaybutton = new Button("Artist Play");
+    Button artistPlaybutton = new Button("Artist Play");
+
+    @FXML
+    private Button backwards;
+
     @FXML
     private Button btnDown;
-Button btnDownbutton = new Button("Down");
+    Button btnDownbutton = new Button("Down");
+
     @FXML
     private Button btnUp;
-Button btnUpbutton = new Button("Up");
+    Button btnUpbutton = new Button("Up");
+
     @FXML
     private Button deletePlay;
-Button deletePlaybutton = new Button("Delete Play");
+    Button deletePlaybutton = new Button("Delete Play");
 
     @FXML
     private Button deleteSong;
@@ -29,26 +43,65 @@ Button deletePlaybutton = new Button("Delete Play");
 
     @FXML
     private Button editPlay;
-
     Button editPlaybutton = new Button("Edit Play");
-
 
     @FXML
     private Button editSong;
-     Button editButton2 = new Button("Edit...");
+    Button editButton2 = new Button("Edit...");
+
+    @FXML
+    private Button forward;
+
+    @FXML
+    private Slider musicPlayer;
+
+    @FXML
+    private Button muteBtn;
+
     @FXML
     private Button newPlay;
- Button newButton = new Button("New...");
+    Button newButton = new Button("New...");
+
+    @FXML
+    private Button playBtn;
+
+    @FXML
+    private TableView<?> playlist;
+
     @FXML
     private Button removeSong;
-Button removeButton = new Button("Remove...");
+    Button removeButton = new Button("Remove...");
+
+    @FXML
+    private Button repeatPlaylist;
+
+    @FXML
+    private TextField searchBar;
+
     @FXML
     private Button searchBtn;
-Button searchButton = new Button("Search...");
+    Button searchButton = new Button("Search...");
+
+    @FXML
+    private Button shuffle;
+
     @FXML
     private Button songOnPlay;
-Button songOnButton = new Button("On Play...");
+    Button songOnButton = new Button("On Play...");
+
     @FXML
     private Text songPlay;
-Button songOnButton2 = new Button("On Play...");
+    Button songOnButton2 = new Button("On Play...");
+
+    @FXML
+    private Text songTimer;
+
+    @FXML
+    private TableView<?> songs;
+
+    @FXML
+    private ListView<?> songsOnPlaylist;
+
+    @FXML
+    private Slider soundDrag;
 }
