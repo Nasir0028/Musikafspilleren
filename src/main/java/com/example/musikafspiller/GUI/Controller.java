1fc881d8d98e7dccd12e36fd6c5c521824de86d0
Stinejenna <stinejenna99@gmail.com>
2024-11-29 10:05:58 +0100

    Fjernet button kode (ikke nødvendigt)

--- src/main/java/com/example/musikafspiller/GUI/Controller.java
+++ src/main/java/com/example/musikafspiller/GUI/Controller.java
@@ -19,10 +19,6 @@ import java.util.List;
 public class Controller {
 
     @FXML
-    private Button addSong;
-    Button addSongbutton = new Button("Add Song");
-
-    @FXML
     private ImageView albumCover;
 
     @FXML
@@ -113,10 +109,9 @@ public class Controller {
 
     @FXML
     private ListView<?> songsOnPlaylist;
-    private ObservableList<?>
 
-    @FXML
-    private Slider soundDrag;
+    //@FXML
+    //private Slider soundDrag;
 
  public void handleAddSong() {
      FileChooser fileChooser = new FileChooser();
