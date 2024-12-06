9a75733be85ba7b5bcbd180be2d5735c20791285
Nasir0028 <143805067+Nasir0028@users.noreply.github.com>
2024-12-06 10:13:35 +0100

    Scenebuilder kommer frem på skærmen

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.example.musikafspiller {
     requires javafx.fxml;
 
 
-    opens com.example.musikafspiller to javafx.fxml;
     exports com.example.musikafspiller.GUI;
     opens com.example.musikafspiller.GUI to javafx.fxml;
 }
